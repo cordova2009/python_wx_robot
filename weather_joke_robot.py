be82bafae5d136384ed245245ff0bb3a029edb77
cordova2009 <875593212@qq.com>
2018-10-22 09:55:46 +0800

    '逻辑优化'

--- weather_joke_robot.py
+++ weather_joke_robot.py
@@ -4,10 +4,15 @@
 import itchat
 import urllib.request
 import requests
-from urllib.parse import quote
+import datetime
+import random
 import string
 import json
+import os
 import re
+from urllib.parse import quote
+from aip import AipSpeech
+
 
 #伪装成谷歌浏览器
 headers=("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
@@ -18,12 +23,46 @@ opener.addheaders = [headers]
 #将operner添加为全局
 urllib.request.install_opener(opener)
 
+#百度语音识别配置类
+class  BaiduSpeech(object):
+    """docstring for  Speech"""
+    def __init__(self):
+        """ 你的 app_id AK SK """
+        self.app_id = 'your'
+        self.api_key = 'your'
+        self.secret_key = 'your'
+
+    #将文字转为语音文件
+    def text_2_voice(self,text):
+        city = text[0:3] or random.randint(0,9)
+        st = datetime.datetime.now().strftime('%Y%m%d')
+        filename = 'weather_%s%s.mp3'%(city,st)
+        filename = quote(filename,safe=string.printable)
+        #判断是否已经生成这个文件
+        if os.path.exists(filename):
+            return filename
+        else:
+            aipSpeech = AipSpeech(self.app_id, self.api_key, self.secret_key)
+            #详细接口参数介绍见百度语音
+            result = aipSpeech.synthesis(text, 'zh', 1, {
+                'vol': 5,
+            })
+            if not isinstance(result,dict):
+                
+                with open(filename,'wb') as f:
+                    f.write(result)
+                    return filename
+        return None
+
+
 class Robot(object):
     """docstring for Robot"""
     def __init__(self):
 
-        #天气地址
+        #天气查询接口
         self.wh_url ="http://tianqi.moji.com/api/redirect/"
+        #天气城市搜索接口
+        self.c_url ="http://tianqi.moji.com/api/citysearch/"
 
         self.joke_url = "http://www.qiushibaike.com/hot/page/"
         #当前页数
@@ -32,10 +71,10 @@ class Robot(object):
         self.num = 1
 
 
-    # 搜索这个城市
-    def search_city(self,city_name):
+    # 根据名称获取这个城市天气查询的url
+    def get_city_url(self,city_name):
         #  构造查询相应城市天气的url  
-        url = "http://tianqi.moji.com/api/citysearch/%s"%city_name
+        url = self.c_url + city_name
         url = quote(url,safe=string.printable)
         text = urllib.request.urlopen(url).read().decode("UTF-8","ignore")
         # 异常捕获
@@ -47,19 +86,23 @@ class Robot(object):
             return city_url
         except:
             print('获取城市错误')
+        return None
 
     def get_weather(self,msg):
         city = msg.Text[-4:-2]
-        url = ""
+        url = None
 
         if city:
-            url = self.search_city(city)
+            url = self.get_city_url(city)
         else:
             city = msg['User']['City']
             if city:
-                url = self.search_city(city)
+                url = self.get_city_url(city)
         #获取url数据
-        if url:
+        if not url:
+            city = city or "深圳"
+            return "我还不知道%s的天气"%city
+        else:
             data = urllib.request.urlopen(url,timeout=10).read().decode("UTF-8","ignore")
             #定义正则
             pat ='(<meta name="description" content=")(.*?)(">)'
@@ -90,7 +133,7 @@ class Robot(object):
                 if 'contentForAll' in j or not j:
                     continue
                 else:
-                    print("笑话",str(self.num),":",j)
+                    print("joke:",str(self.num),":",j)
                     self.num += 1
                     self.page += 1
                     j = j.replace("<span>","").replace("</span>","")
@@ -101,13 +144,22 @@ class Robot(object):
 
 
 bot = Robot()
+speech = BaiduSpeech()
 # itchat.send(msg,toUserName = userName) ##发送信息给个人,data就是爬取的内容
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
     if '笑话' in msg['Text']:
         itchat.send(bot.talk_joke(), msg['FromUserName'])
     elif '天气' in msg['Text']:
-        itchat.send(bot.get_weather(msg), msg['FromUserName'])
+        text = bot.get_weather(msg)
+        if text:
+            file = speech.text_2_voice(text)
+            if file:
+                itchat.send_file(file, msg['FromUserName'])
+            else:
+                itchat.send("api接口错误", msg['FromUserName'])
+        else:
+            itchat.send("未查询到相应天气信息", msg['FromUserName'])
    
 
 if __name__ == '__main__':
